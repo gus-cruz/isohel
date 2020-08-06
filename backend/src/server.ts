3cf59efbac6b76f23b784c73aa8ce2c14f7087b8
Gustavo Ferreira <gustavofrc8@gmail.com>
2020-08-06 19:18:54 -0300

    database created

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,5 @@
-import express from 'express';
-
-const app = express();
-
-app.get('', (request, response) => {
-    return response.json({ message: 'isohel' });
-});
+import app from './app';
 
 app.listen(3333, () => {
-    console.log('Server started on port 3333! 🤏');
+    console.log('⏳ Server started on port 3333!');
 });
